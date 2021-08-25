45e1fb7743728f439fbfa4f3caf26a1136e04b36
vhtorres33 <89109562+vhtorres33@users.noreply.github.com>
2021-08-24 23:48:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Debte - 3 Tratamiento de datos</p>"
 
 
 @app.route("/get-price/<ticker>")
 def get_price(ticker):
     url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
-    response = requests.get(url)
+    headers={'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url,headers=headers)
     company_info = response.json()
-    app.logger.info(f"Requested ticker: {ticker}")
 
     if response.status_code > 400:
         app.logger.info(f"Yahoo has problem with ticker: {ticker}.")
